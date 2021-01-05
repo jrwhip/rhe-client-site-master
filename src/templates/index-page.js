1329dc4397db3073151ffae5103143b3173c772b
All Knowing Sock <aks@pop-os.localdomain>
2021-01-04 23:18:46 -0700

    Added Bootstrap

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -16,6 +16,15 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
+    <button className="btn btn-primary">TEST BUTTON</button>
+    <button type="button" className="btn btn-primary">Primary</button>
+    <button type="button" className="btn btn-secondary">Secondary</button>
+    <button type="button" className="btn btn-success">Success</button>
+    <button type="button" className="btn btn-danger">Danger</button>
+    <button type="button" className="btn btn-warning">Warning</button>
+    <button type="button" className="btn btn-info">Info</button>
+    <button type="button" className="btn btn-light">Light</button>
+    <button type="button" className="btn btn-dark">Dark</button>
     <div
       className="full-width-image margin-top-0"
       style={{
